f335f5fa89b76e27068b9649aa3c875e79dbda5e
parker <qkrrudtjr954@kangwon.ac.kr>
2017-10-11 19:55:47 +0900

    communicate with client

--- src/com/rsp/server/Main.java
+++ src/com/rsp/server/Main.java
@@ -1,8 +1,6 @@
 package com.rsp.server;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
@@ -11,6 +9,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args){
         ServerSocket serverSocket = null;
+        Socket socket = null;
         try{
             serverSocket = new ServerSocket(5000);
             System.out.println(getTime() + " server is running... ");
@@ -21,10 +20,32 @@ public class Main {
         while(true){
             try{
                 System.out.println(getTime() + "Wait Request...");
+
                 //서버소켓은 클라이언트의 연결요청이 올 때까지 실행을 멈추고 기다린다.
                 //클라인언트의 연결요청이 오면 클라이언트 소켓과 통신할 새로운 소켓을 생성한다.
-                Socket socket = serverSocket.accept();
+                socket = serverSocket.accept();
                 System.out.println(getTime() + "connect from "+ socket.getInetAddress() + " ...");
+
+                InputStream in = socket.getInputStream();
+                DataInputStream dis = new DataInputStream(in);
+
+                while(true){
+                    int data = dis.read();
+                    if (data == -1){
+                        break;
+                    }
+                    System.out.println(data);
+
+                }
+                dis.close();
+                socket.close();
+            } catch(IOException ex){
+                ex.printStackTrace();
+            }
+
+            try{
+                socket = serverSocket.accept();
+                System.out.println(getTime() + "connect2 from "+ socket.getInetAddress() + " ...");
 
                 //소켓의 출력 스트림을 얻는다.
                 OutputStream out = socket.getOutputStream();
@@ -35,10 +56,11 @@ public class Main {
 
                 dos.close();
                 socket.close();
-
             } catch(IOException ex){
                 ex.printStackTrace();
             }
+
+
         }
     }
     static String getTime(){
